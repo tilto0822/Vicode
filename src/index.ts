2ca3927cb5119ef093679817e02a78ea1c243f5f
Tilto_ <tilto0822@gmail.com>
2022-11-06 22:04:20 +0900

    🔖 0.0.4

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
 import ViCode from './ViCode';
 
-import BaseNode from 'node/BaseNode';
-import NodeConnection from 'node/NodeConnection';
-import NodeManager from 'node/NodeManager';
-import NodePosition from 'node/NodePosition';
-import NodeShape from 'node/NodeShape';
-import NodeStyle from 'node/NodeStyle';
+import BaseNode from './node/BaseNode';
+import NodeConnection from './node/NodeConnection';
+import NodeManager from './node/NodeManager';
+import NodePosition from './node/NodePosition';
+import NodeShape from './node/NodeShape';
+import NodeStyle from './node/NodeStyle';
 
 export { BaseNode, NodeConnection, NodeManager };
 export type { NodePosition, NodeShape, NodeStyle };
 
-import BaseInterface from 'node/interface/BaseInterface';
-import CachedData from 'node/interface/CachedData';
-import DataInput from 'node/interface/DataInput';
-import DataInputVariant from 'node/interface/DataInputVariant';
-import DataInterface from 'node/interface/DataInterface';
-import FlowInterface from 'node/interface/FlowInterface';
+import BaseInterface from './node/interface/BaseInterface';
+import CachedData from './node/interface/CachedData';
+import DataInput from './node/interface/DataInput';
+import DataInputVariant from './node/interface/DataInputVariant';
+import DataInterface from './node/interface/DataInterface';
+import FlowInterface from './node/interface/FlowInterface';
 
 export { BaseInterface, DataInterface, FlowInterface };
 export type { CachedData, DataInput, DataInputVariant };
 
-import BaseCategory from 'category/BaseCategory';
-import CategoryManager from 'category/CategoryManager';
+import BaseCategory from './category/BaseCategory';
+import CategoryManager from './category/CategoryManager';
 
 export { BaseCategory, CategoryManager };
 
